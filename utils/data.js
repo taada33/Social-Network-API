b6f5e40df72e8bb33b0ef4b51c42251cb2f77c3e
Thomas <taadamson33@gmail.com>
2023-04-15 02:21:37 -0400

    added seed data and seeding script

---
+++ utils/data.js
@@ -0,0 +1,52 @@
+const userData = [
+    {
+        username: "Spike",
+        email: "Bang.@email.com",
+        thoughts: [],
+        friends: [],
+    },
+    {
+        username: "Jet",
+        email: "bebop@email.com",
+        thoughts: [],
+        friends: [],
+    },
+    {
+        username: "Faye",
+        email: "valentine@email.com",
+        thoughts: [],
+        friends: [],
+    },
+    {
+        username: "Ed",
+        email: "woof@email.com",
+        thoughts: [],
+        friends: [],
+    },
+];
+
+const thoughtData = [
+    {
+        thoughtText: "You know the first rule of combat? Shoot them before they shoot you.",
+        username: "Faye",
+        reactions: [],
+    },
+    {
+        thoughtText: "So Charlie says, 'only hands can wash hands. If you want to receive, you have to give.",
+        username: "Jet",
+        reactions: [],
+    },
+    {
+        thoughtText: "Whatever happens, happens.",
+        username: "Spike",
+        reactions: [],
+    },
+    {
+        thoughtText: "FOOD! Gimme, gimme, gimme!",
+        username: "Ed",
+        reactions: [],
+    },
+    
+];
+
+module.exports = { userData, thoughtData };
